b3dc1402a9c0ce5c30b6bc255fde1372db1c133a
wawrze <mateusz.wawreszuk@gmail.com>
2018-11-01 20:18:13 +0100

    repository initialisation

---
+++ src/main/java/com/wawrze/ads/AlgorithmsAndDataStructuresApplication.java
@@ -0,0 +1,12 @@
+package com.wawrze.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsAndDataStructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsAndDataStructuresApplication.class, args);
+    }
+}
